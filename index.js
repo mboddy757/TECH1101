6acc7dc91bb5ed699a19a571f2ab659b6d5ba193
Michael Boddy <mboddy757@users.noreply.github.com>
2020-02-28 19:49:56 -0700

    Cleanup and remove console.logs

--- index.js
+++ index.js
@@ -10,18 +10,12 @@ submitBtnRef.addEventListener("click", submitIncomeFun);//listens for the submit
 
 function submitIncomeFun(){//function to accept user input from income text box
     if(isNaN(parseFloat(incomeRef.value))){//if true user has entered a non number
-        //console.log(incomeRef.value);
-        console.log("not a damn number");
         postErrorMessage();
     }
     else{//if true user has entered a number
         const income =parseFloat(incomeRef.value);
-        console.log("is a damn number");
-        //console.log(income);
-        //console.log("typeof income", typeof(income));  
-        const totalTax=calculateTax(income).toFixed(2);
-        console.log(totalTax);
-        postTax(totalTax);
+        const totalTax=calculateTax(income);
+        postTax(totalTax.toFixed(2));
         const effectiveTaxRate=(totalTax/income*100).toFixed(2);
         postEffectiveTaxRate(effectiveTaxRate);
         const retainedEarnings=(income-totalTax).toFixed(2);
@@ -64,11 +58,11 @@ function removeErrorMessage(){
 //calcaulte tax code
 function calculateTax(income){
     const taxBracket1Max=48535;//tax bracket thresholds are stored as const varibles to make future updating eaisier
-    const taxBracket2Max=97069;
+    const taxBracket2Max=97069;//could use an array, but ran out of time
     const taxBracket3Max=150473;
     const taxBracket4Max=214368;
     const taxBracket1Rate=0.15;//tax bracket rates are stored as const varibles to make future updating eaisier
-    const taxBracket2Rate=0.205;
+    const taxBracket2Rate=0.205;//could use an array, but ran out of time
     const taxBracket3Rate=0.26;
     const taxBracket4Rate=0.29;
     const taxBracket5Rate=0.33;
@@ -78,37 +72,28 @@ function calculateTax(income){
     else{//income is positive number
         if (income<=taxBracket1Max){//income is within tax bracket 1
             tax=income*taxBracket1Rate;//calculates tax on income within tax bracket 1
-            console.log("within tax bracket 1");
         }
         else{//income exceeds tax bracket 1
             tax=taxBracket1Max*taxBracket1Rate;//tax for bracket 1 is maximum
-            console.log("maximum tb1 tax applied");
 
             if(income<=taxBracket2Max){//income is within tax bracket 2
                 tax+=(income-taxBracket1Max)*taxBracket2Rate;//calculates tax on income within tax bracket 2
-                console.log("within tax bracket 2");
             }
             else{//income exceeds tax bracket 2 
                 tax+=(taxBracket2Max-taxBracket1Max)*taxBracket2Rate;//tax for bracket 2 is maximum
-                console.log("maximum tb2 tax applied");
             
                 if(income<=taxBracket3Max){//income is within tax bracket 3
                     tax+=(income-taxBracket2Max)*taxBracket3Rate;//calculates tax on income within tax bracket 3
-                    console.log("within tax bracket 3");
                 }
                 else{//income exceeds tax bracket 3
                     tax+=(taxBracket3Max-taxBracket2Max)*taxBracket3Rate;//tax for bracket 3 is maximum
-                    console.log("maximum tb3 tax applied");
                 
                     if((income<=taxBracket4Max)){//income is within tax bracket 4
                         tax+=(income-taxBracket3Max)*taxBracket4Rate;//calculates tax on income within tax brackets 4
-                        console.log("within tax bracket 4");
                     } 
                     else{//income exceeds tax bracket 4 and is therefore within tax bracket 5
                         tax+=(taxBracket4Max-taxBracket3Max)*taxBracket4Rate;//tax for bracket 4 is maximum
-                        console.log("maximum tb4 tax applied");
-                        tax+=(income-taxBracket4Max)*taxBracket5Rate;//calculates tax on income within tax bracket 5 
-                        console.log("within tax bracket 5");        
+                        tax+=(income-taxBracket4Max)*taxBracket5Rate;//calculates tax on income within tax bracket 5    
                     }
                 }  
             }
@@ -119,46 +104,46 @@ function calculateTax(income){
 
 //post tax code
 const taxOwedRef=document.getElementById("taxOwed");
+
 function postTax(tax){
     removePostTax();
     const newNode = document.createElement("h3");
     const newTextNode = document.createTextNode("Tax owed: $"+tax);
     newNode.appendChild(newTextNode);
-    taxOwedRef.appendChild(newNode);//.setAttribute("id", "1");//ID part is NOT NEEDED
+    taxOwedRef.appendChild(newNode);
 }
 
-//remove tax code
-function removePostTax(){
+function removePostTax(){//remove tax code
     while(taxOwedRef.hasChildNodes()){
         taxOwedRef.removeChild(taxOwedRef.childNodes[0]);    
     }
 }
-function resetTax(){
+
+function resetTax(){//reset tax code
     const newNode = document.createElement("h3");
     const newTextNode = document.createTextNode("Tax owed:");
     newNode.appendChild(newTextNode);
-    taxOwedRef.appendChild(newNode);//.setAttribute("id", "1");//ID part is NOT NEEDED
+    taxOwedRef.appendChild(newNode);
 }
-
 
 //post effective tax rate code
 const effectiveTaxRateRef=document.getElementById("effectiveTaxRate");
-function postEffectiveTaxRate(effectiveTaxRate){
-    //effectiveTaxRateRef.removeChild(effectiveTaxRateRef.childNodes[1]);    
+
+function postEffectiveTaxRate(effectiveTaxRate){ 
     removeEffectiveTaxRate();
     const newNode = document.createElement("h3");
     const newTextNode = document.createTextNode("Effective tax Rate: "+effectiveTaxRate+"%");
     newNode.appendChild(newTextNode);
-    effectiveTaxRateRef.appendChild(newNode);//.setAttribute("id", "1"//ID part is NOT NEEDED
+    effectiveTaxRateRef.appendChild(newNode);
 }
 
-function removeEffectiveTaxRate(){
+function removeEffectiveTaxRate(){//remove effective tax rate
     while(effectiveTaxRateRef.hasChildNodes()){
         effectiveTaxRateRef.removeChild(effectiveTaxRateRef.childNodes[0]);    
     }
 }
 
-function resetTaxRate(){
+function resetTaxRate(){//reset effective tax rate
     const newNode = document.createElement("h3");
     const newTextNode = document.createTextNode("Effective tax Rate:");
     newNode.appendChild(newTextNode);
@@ -167,21 +152,21 @@ function resetTaxRate(){
 
 //post retained earnings code
 const retainedEarningsRef=document.getElementById("retainedEarnings");
+
 function postRetainedEarnings(retainedEarnings){
-    //retainedEarningsRef.removeChild(retainedEarningsRef.childNodes[1]);    
     removeRetainedEarnings()
     const newNode = document.createElement("h3");
     const newTextNode = document.createTextNode("Retained Earnings: $"+retainedEarnings);
     newNode.appendChild(newTextNode);
-    retainedEarningsRef.appendChild(newNode);//.setAttribute("id", "1"//ID part is NOT NEEDED
+    retainedEarningsRef.appendChild(newNode);
 }
 
-function removeRetainedEarnings(){
+function removeRetainedEarnings(){//remove retained earnings
     while(retainedEarningsRef.hasChildNodes()){
         retainedEarningsRef.removeChild(retainedEarningsRef.childNodes[0]);    
     }
 }
-function resetRetainedEarnings(){
+function resetRetainedEarnings(){//reset retained earnings
     const newNode = document.createElement("h3");
     const newTextNode = document.createTextNode("Retained Earnings:");
     newNode.appendChild(newTextNode);
